6640e68d4d2065bba2edf46275a6d1a937d48449
vasiliy.stan <vasiliyStankevich@gmail.com>
2023-09-07 03:21:56 +0300

    update code for  FirstTest.cs

--- Sources/ApiProxy.Tests/FirstTest.cs
+++ Sources/ApiProxy.Tests/FirstTest.cs
@@ -1,22 +1,67 @@
 ﻿using ApiProxy.Logic.Refactoring;
 using ApiProxy.Logic.Refactoring.Logic;
+using ApiProxy.Logic.Refactoring.Models;
+using AutoFixture.Xunit2;
+using Moq;
+using Tests.AAAPattern.xUnit.Attributes;
 using Xunit;
 
 namespace ApiProxy.Tests;
 
 public class FirstTest
 {
-    [Fact]
-    public void ResolivingServiceLocatorTest()
+    //[Fact]
+    //public void RealResolivingServiceLocatorTest()
+    //{
+    //    var serviceLocator = ServiceLocator.GetInstance();
+
+    //    var registrator = new RegistratorTypes();
+    //    registrator.RegisterAllTypes(serviceLocator);
+    //    var mobileStore = serviceLocator.Resolve<IMobileStore>();
+
+    //    mobileStore.Process();
+
+    //    Assert.True(true);
+    //}
+
+    [Theory]
+    [MoqInlineAutoData("store.txt", true)]
+    [MoqInlineAutoData("store.txt", false)]
+    public void AaaPatternResolivingServiceLocatorTest(string fileName, bool phoneValidValue, [Frozen] Mock<IServiceLocator> serviceLocator, 
+        Mock<IPhoneReader> phoneReader, Mock<IPhoneBinder> phoneBinder, Mock<IPhoneValidator> phoneValidator,
+        Mock<IPhoneSaver> phoneSaver, Mock<MobileStore> mobileStore, Phone modelPhone)
     {
-        var serviceLocator = ServiceLocator.GetInstance();
+        //Arrange
+        var phoneData = new[] { modelPhone.Model, $"{modelPhone.Price}" };
+        
+        serviceLocator.Setup(e => e.Resolve<IPhoneReader>(string.Empty)).Returns(phoneReader.Object);
+        serviceLocator.Setup(e => e.Resolve<IPhoneBinder>(string.Empty)).Returns(phoneBinder.Object);
+        serviceLocator.Setup(e => e.Resolve<IPhoneValidator>(string.Empty)).Returns(phoneValidator.Object);
+        serviceLocator.Setup(e => e.Resolve<IPhoneSaver>(string.Empty)).Returns(phoneSaver.Object);
+        serviceLocator.Setup(e => e.Resolve<IMobileStore>(string.Empty)).Returns(mobileStore.Object);
 
-        var registrator = new RegistratorTypes();
-        registrator.RegisterAllTypes(serviceLocator);
-        var mobileStore = serviceLocator.Resolve<IMobileStore>();
+        phoneReader.Setup(e => e.GetInputData()).Returns(phoneData);
+        phoneBinder.Setup(e => e.CreatePhone(phoneData)).Returns(modelPhone);
+        phoneValidator.Setup(e => e.IsValid(modelPhone)).Returns(phoneValidValue);
+        if (phoneValidValue)
+            phoneSaver.Setup(e => e.Save(modelPhone, fileName));
 
-        mobileStore.Process();
+        var ms = serviceLocator.Object.Resolve<IMobileStore>();
 
-        Assert.True(true);
+        //Act
+        ms.Process();
+
+        //Assert
+        serviceLocator.Verify(e=>e.Resolve<IPhoneReader>(""), Times.Once);
+        serviceLocator.Verify(e => e.Resolve<IPhoneBinder>(""), Times.Once);
+        serviceLocator.Verify(e => e.Resolve<IPhoneValidator>(""), Times.Once);
+        serviceLocator.Verify(e => e.Resolve<IPhoneSaver>(""), Times.Once);
+        serviceLocator.Verify(e => e.Resolve<IMobileStore>(""), Times.Once);
+
+        phoneReader.Verify(e => e.GetInputData(), Times.Once);
+        phoneBinder.Verify(e => e.CreatePhone(phoneData), Times.Once);
+        phoneValidator.Verify(e => e.IsValid(modelPhone), Times.Once);
+        if (phoneValidValue)
+            phoneSaver.Verify(e => e.Save(modelPhone, fileName), Times.Once);
     }
 }
