9256d484c02208e76e1e49eeb2ef9ee61905f0fd
vasiliy.stan <vasiliyStankevich@gmail.com>
2023-09-17 02:06:20 +0300

    work on clear code

--- Sources/ApiProxy.Logic/ClearCode/Logic/PrimeNumberGenerator.cs
+++ Sources/ApiProxy.Logic/ClearCode/Logic/PrimeNumberGenerator.cs
@@ -2,11 +2,9 @@
 {
     public class PrimeNumberGenerator
     {
-        public void Init(int ordMax, int m)
+        public PrimeNumberGenerator(PrimesData primesData)
         {
-            Mult = new int[ordMax + 1];
-            P = new int[m + 1];
-            P[1] = 2;
+            PrimesData = primesData;
         }
 
         public int [] Calc(int m)
@@ -15,54 +13,22 @@
             var k = 1;
             while (k < m)
             {
-                prime = GetPrimeNumber(prime);
-                P[++k] = prime;
+                var ord = 2;
+                var square = 9;
+                var isPrime = false;
+
+                do
+                {
+                    prime += 2;
+                    if (prime == square) square = PrimesData.GetSquare(prime, ref ord);
+                    else isPrime = PrimesData.IsPrime(prime, ord);
+                } while (!isPrime);
+
+                PrimesData.SetPrime(k++, prime);
             }
-            return P;
+            return PrimesData.P;
         }
 
-        int GetPrimeNumber(int prime)
-        {
-            var ord = 2;
-            var square = 9;
-            var isPrime=false;
-
-            do
-            {
-                prime += 2;
-                if (prime == square)
-                    square = GetSquare(prime, ref ord);
-                else
-                    isPrime = IsPrime(prime, ord);
-            } while (!isPrime);
-
-            return prime;
-        }
-
-        private bool IsPrime(int prime, int ord)
-        {
-            var n = 2;
-            var isPrime = true;
-            while (n < ord && isPrime)
-            {
-                while (Mult[n] < prime)
-                    Mult[n] += 2 * P[n];
-                if (Mult[n] == prime)
-                    isPrime = false;
-                n++;
-            }
-            return isPrime;
-        }
-
-        int GetSquare(int prime, ref int ord)
-        {
-            ord++;
-            Mult[ord - 1] = prime;
-            return P[ord] * P[ord];
-        }
-
-
-        public int[] P { get; set; }
-        public int[] Mult { get; set; }
+        PrimesData PrimesData { get; }
     }
 }
--- Sources/ApiProxy.Logic/ClearCode/Logic/PrimesData.cs
+++ Sources/ApiProxy.Logic/ClearCode/Logic/PrimesData.cs
@@ -2,8 +2,41 @@
 {
     public class PrimesData
     {
-        public PrimesData()
+        public PrimesData(int ordMax, int m)
         {
+            Mult = new int[ordMax + 1];
+            P = new int[m + 1];
+            P[1] = 2;
         }
+
+        public bool IsPrime(int prime, int ord)
+        {
+            var n = 2;
+            var isPrime = true;
+            while (n < ord && isPrime)
+            {
+                while (Mult[n] < prime)
+                    Mult[n] += 2 * P[n];
+                if (Mult[n] == prime)
+                    isPrime = false;
+                n++;
+            }
+            return isPrime;
+        }
+
+        public int GetSquare(int prime, ref int ord)
+        {
+            ord++;
+            Mult[ord - 1] = prime;
+            return P[ord] * P[ord];
+        }
+
+        public void SetPrime(int k, int prime)
+        {
+            P[k] = prime;
+        }
+
+        public int[] P { get; }
+        int[] Mult { get; }
     }
 }
